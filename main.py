81cdd5714a05054a3e814cd88f6b62d7c2744c41
marbh56 <nathan.a.sanders2@gmail.com>
2024-03-03 14:08:28 -0500

    3/3 work

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+from tkinter import *
+from tkinter import ttk
+
 def fib(index):
 	n = 0
 	sequence = []
 	sequence.append(1)
 	sequence.append(1)
-	while n != index:
+	while index != len(sequence):
 		sequence.append(sequence[n] + sequence[n+1])
-		n += 1
-	#print(sequence)
+		n+=1
+	print(sequence)
 
-
-	
-
-
+def fib_callback():
+	return fib(int(num_of_fib_iters.get()))
 
 
 def main():
-	fib(80)
+	fib(10)
+	root = tk.Tk()
+	num_of_fib_iters = Entry(root)
+	num_of_fib_iters.pack()
+	b = Button(root, text="Num of Iterations to calculate the Fibonacci Sequence?", command=fib_callback)
+	b.pack()
+	
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 	main()
